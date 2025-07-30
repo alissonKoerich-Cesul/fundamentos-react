9660bf770a628f85924d9fdb1d8369f722342f1e
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-07-29 21:51:45 -0300

    chore: theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import {defineConfig } from "@chakra-ui/react";
+
+export const themeconfig = defineConfig({
+  globalCss:{
+    "html, body": {
+      backgroundColor: "gray.50"
+  },
+},
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+})
