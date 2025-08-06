e05161eeebabdb4a960b57ce60aec102168e55d9
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-08-05 21:37:13 -0300

    feat: layout login finished

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,7 @@
 import {
+  Button,
+  Link as ChakraLink,
+  Field,
   Flex,
   Heading,
   HStack,
@@ -8,7 +11,9 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { Checkbox } from "@/components/ui/checkbox";
+import { PasswordInput } from "@/components/ui/password-input";
 import loginImage from "../../public/assets/login-image.gif";
 
 export default function Login() {
@@ -17,22 +22,48 @@ export default function Login() {
       <Flex w="50%" bg="#2C73EB" align="center" justify="center">
         <Image w={500} h={500} src={loginImage.src} />
       </Flex>
-      <VStack w="50%" bg="green.400" justify="center">
+      <VStack w="50%" justify="center">
         <Stack>
-          <Heading as="h1">Account Login</Heading>
-          <Text>
-            If you are already a member you can login with your email address
-            and password.
+          <Heading as="h1" color={"#000000"} fontSize="3xl" fontWeight="bold ">
+            Login
+          </Heading>
+          <Text color="gray.500" fontSize="lg" fontWeight={"normal"}>
+            Se você já é membro, pode fazer login com seu endereço de e-mail e
+            senha.
           </Text>
 
-          <Input />
+          <VStack align="flex-start" gap={6} mt={10}>
+            <Field.Root>
+              <Field.Label color="gray.500">Email</Field.Label>
+              <Input  h={16} colorPalette="blue"  borderRadius="md" color={"black"}
+              />
+            </Field.Root>
 
-          <Input />
+            <Field.Root colorPalette={"black"}>
+              <Field.Label color="gray.500">Senha</Field.Label>
+              <PasswordInput type="password" h={16} colorPalette="blue" borderRadius="md" color={"black"}
+              />
+            </Field.Root>
 
-          <Checkbox>
-            Lembre-me
-          </Checkbox>
+            <Checkbox colorPalette="blue" color="gray.500" fontSize={"md"} fontWeight={"medium"}
+            >
+              Lembre-me
+            </Checkbox>
 
+            <Button w="full" h={16} colorPalette="blue" borderRadius="md" fontWeight="medium" fontSize="md">
+              Entrar
+            </Button>
+          </VStack>
+
+          <HStack gap={1} justify={"center"} mt={10}>
+            <Text color="gray.500" fontWeight="medium" fontSize="md">
+              Não possui uma conta?
+            </Text>
+
+            <ChakraLink color="blue" asChild>
+              <NextLink href="/sign-up">Clique aqui!</NextLink>
+            </ChakraLink>
+          </HStack>
         </Stack>
       </VStack>
     </Flex>
