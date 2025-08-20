3ac7c578c421aa4b8f194a10a6a4ac4c7a36a8e9
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-08-19 22:29:59 -0300

    feat: header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Heading } from "@chakra-ui/react";
+import { DefaultLayout } from "@/layouts/DefaultLayout";
+
 export default function Home() {
   return (
-    <div>
-      Hello World!
-    </div>
-  )
+    <DefaultLayout>
+      <Heading color={"red"}> DASHBOARD DO BIANAKTI</Heading>
+    </DefaultLayout>
+  );
 }
