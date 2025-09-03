f2c27ea491aac1040e51f19ef643c47391b116ee
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-09-02 22:36:39 -0300

    feat: cards

---
+++ src/components/Card/cardIndicator.tsx
@@ -0,0 +1,19 @@
+import { Card, Flex, FormatNumber, Icon, Stat } from "@chakra-ui/react";
+
+
+export default function CardIndicator() {
+  return(
+
+    <Card.Root>
+
+    </Card.Root>
+
+
+
+  );
+
+
+
+
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { Card, Flex, FormatNumber, HStack, Icon, Stat } from "@chakra-ui/react";
+import { FaBookOpen } from "react-icons/fa";
+import { FaArrowTrendUp } from "react-icons/fa6";
 import { IoMdPeople } from "react-icons/io";
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 
 export default function Home() {
@@ -8,7 +11,7 @@ export default function Home() {
       title="DASHBOARD ACADEMICO"
       description="Visão geral do sistema universitário"
     >
-    <HStack>
+      <HStack>
         <Card.Root>
           <Card.Body flexDir="row" alignItems="center" gap={6}>
             <Stat.Root>
@@ -16,7 +19,9 @@ export default function Home() {
               <Stat.ValueText>
                 <FormatNumber value={1340} />
               </Stat.ValueText>
-              <Stat.UpIndicator alignSelf="start">+12% este mes</Stat.UpIndicator>
+              <Stat.UpIndicator alignSelf="start">
+                +12% este mes
+              </Stat.UpIndicator>
             </Stat.Root>
 
             <Flex
@@ -32,7 +37,85 @@ export default function Home() {
             </Flex>
           </Card.Body>
         </Card.Root>
-    </HStack>
+
+        <Card.Root>
+          <Card.Body flexDir="row" alignItems="center" gap={6}>
+            <Stat.Root>
+              <Stat.Label>Total de Professores</Stat.Label>
+              <Stat.ValueText>
+                <FormatNumber value={121} />
+              </Stat.ValueText>
+              <Stat.UpIndicator alignSelf="start">
+                +1% este mes
+              </Stat.UpIndicator>
+            </Stat.Root>
+
+            <Flex
+              justify={"center"}
+              align="center"
+              backgroundColor={"yellow.100"}
+              rounded={"lg"}
+              p={4}
+            >
+              <Icon size="xl" color={"yellow.700"}>
+                <LiaChalkboardTeacherSolid />
+              </Icon>
+            </Flex>
+          </Card.Body>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Body flexDir="row" alignItems="center" gap={6}>
+            <Stat.Root>
+              <Stat.Label>Cursos Oferecidos</Stat.Label>
+              <Stat.ValueText>
+                <FormatNumber value={24} />
+              </Stat.ValueText>
+              <Stat.UpIndicator alignSelf="start">
+                +1% este mes
+              </Stat.UpIndicator>
+            </Stat.Root>
+
+            <Flex
+              justify={"center"}
+              align="center"
+              backgroundColor={"blue.100"}
+              rounded={"lg"}
+              p={4}
+            >
+              <Icon size="xl" color={"purple.700"}>
+                <FaBookOpen />
+              </Icon>
+            </Flex>
+          </Card.Body>
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Body flexDir="row" alignItems="center" gap={6}>
+            <Stat.Root>
+              <Stat.Label>Taxa de Aprovação</Stat.Label>
+              <Stat.ValueText>
+                <FormatNumber value={24} />
+              </Stat.ValueText>
+              <Stat.UpIndicator alignSelf="start">
+                +2.8% este mes
+              </Stat.UpIndicator>
+            </Stat.Root>
+
+            <Flex
+              justify={"center"}
+              align="center"
+              backgroundColor={"blue.100"}
+              rounded={"lg"}
+              p={4}
+            >
+              <Icon size="xl" color={"red.700"}>
+                <FaArrowTrendUp />
+              </Icon>
+            </Flex>
+          </Card.Body>
+        </Card.Root>
+      </HStack>
     </DefaultLayout>
   );
 }
