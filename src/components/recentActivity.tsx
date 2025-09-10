add88a46720c4a0df71be74b4263c599bbf883eb
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-09-09 22:26:44 -0300

    create recentActivity

---
+++ src/components/recentActivity.tsx
@@ -0,0 +1,36 @@
+import { Card, Flex, Heading, HStack, Icon, Text, VStack } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+
+
+interface RecentActivityProps {
+  title: string;
+  icon: IconType;
+  colorPallet: string;
+  dateTime: string;
+
+
+}
+export function RecentActivity({title, icon, colorPallet, dateTime}: RecentActivityProps) {
+  return(
+
+    <HStack>
+        <Flex
+          justify={"center"}
+          align={"center"}
+          backgroundColor={`${colorPallet}.100`}
+          rounded={"lg"}
+          p={2}
+        >
+          <Icon as={icon} size="xl" color={`${colorPallet}.700`}></Icon>
+        </Flex>
+        <VStack align={"start"} gap={1}>
+          <Heading>
+            {title}
+          </Heading>
+          <Text fontSize={"sm"} color={"gray.500"}>
+            {dateTime}
+          </Text>
+        </VStack>
+   </HStack>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,49 @@
-import { Card, Flex, FormatNumber, HStack, Icon, Stat } from "@chakra-ui/react";
+import { Card, HStack } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { BiAward, BiCalendar } from "react-icons/bi";
 import { FaBookOpen, FaGraduationCap } from "react-icons/fa";
 import { FaArrowTrendUp } from "react-icons/fa6";
-import { IoMdPeople } from "react-icons/io";
-import { LiaChalkboardTeacherSolid } from "react-icons/lia";
+import { IoIosBook, IoMdPeople } from "react-icons/io";
 import { IndicatorCard } from "@/components/indicatorCard";
+import { RecentActivity } from "@/components/recentActivity";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 
-
 export default function Home() {
+  const recentActivities = [
+    {
+      id: 1,
+      type: "enrollment",
+      title: "Nova matrícula: Maria Silva - Engenharia de Software",
+      time: "2 min atrás",
+    },
+    {
+      id: 2,
+      type: "grade",
+      title: "Notas lançadas: Cálculo I - Prof. João Santos",
+      time: "15 min atrás",
+    },
+    {
+      id: 3,
+      type: "course",
+      title: "Novo curso criado: Análise de Ddoas",
+      time: "1 hora atrás",
+    },
+    {
+      id: 4,
+      type: "calendar",
+      title: "Evento adicionado: Semana da Computação",
+      time: "2 min atrás",
+    },
+  ];
+
+  const iconMap: Record<string, IconType> = {
+    enrollment: IoMdPeople,
+    grade: BiAward,
+    course: IoIosBook,
+    calendar: BiCalendar,
+  };
+
   return (
     <DefaultLayout
       title="DASHBOARD ACADEMICO"
@@ -22,7 +58,7 @@ export default function Home() {
           colorPallet="blue"
         />
 
-           <IndicatorCard
+        <IndicatorCard
           label="Total de Professores"
           value={121}
           indicator="+1% este mes"
@@ -30,7 +66,7 @@ export default function Home() {
           colorPallet="yellow"
         />
 
-           <IndicatorCard
+        <IndicatorCard
           label="Cursos Oferecidos"
           value={24}
           indicator="-2 este mes"
@@ -38,7 +74,7 @@ export default function Home() {
           colorPallet="purple"
         />
 
-            <IndicatorCard
+        <IndicatorCard
           label="Taxa de Aprovação"
           value={0.892}
           style="percent"
@@ -48,9 +84,29 @@ export default function Home() {
           icon={FaArrowTrendUp}
           colorPallet="red"
         />
+      </HStack>
 
+      <HStack mt={8}>
+        <Card.Root>
+          <Card.Header>
+            <Card.Title>Atividades Recentes</Card.Title>
+          </Card.Header>
+          <Card.Body gap={10}>
+            {recentActivities.map((activity) => {
+              const ActivityIcon = iconMap[activity.type] ?? AiFillQuestionCircle;
+              return(
+                   <RecentActivity
+                      key={activity.id}
+                      dateTime={"2 minutos atrás"}
+                      icon={ActivityIcon}
+                      title={activity.title}
+                      colorPallet="blue"
+            />
+            )}
+          )}
 
-
+          </Card.Body>
+        </Card.Root>
       </HStack>
     </DefaultLayout>
   );
